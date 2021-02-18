3a8765c36ef53e191766ffa1ffe3a7f5709c8608
NakshatraCodes <nakshatra163@gmail.com>
2021-02-18 23:54:18 +0530

    Allow Cross Domains

--- index.js
+++ index.js
@@ -38,6 +38,15 @@ mongoose.connect(
 
 app.use(express.json());
 
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
+app.use(allowCrossDomain);
+
 app.use("/api/", apiLimiter);
 app.use("/", userRoutes);
 app.use("/api", apiRoutes);
